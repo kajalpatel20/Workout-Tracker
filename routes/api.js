c50672e05fa132b98e4ee03e4638bf4939d3c11b
Kajal Patel <kajalpatel20@gmail.com>
2021-04-21 18:30:08 -0400

    deploying

--- routes/api.js
+++ routes/api.js
@@ -2,49 +2,16 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const { route } = require("./htmlRoutes.js");
 
-
-router.get("/api/workouts", (req, res) => {
-  Workout.aggregate([
-      {
-          $addFields: {
-              totalDuration: {
-                  $sum: '$exercise.duration'
-              },
-          },
-      },
-  ])
-  .then((workout) => {
-      console.log('YOO', workout);
-      res.json(workout)
-  })
-  .catch((e) => {
-      res.json(e)
-  })
+router.post("/workouts", ({ body }, res) => {
+    Workout.create({})
+        .then((workout) => {
+            res.json(workout)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
 });
-router.post("/api/workouts", (req, res) => {
-        Workout.create({})
-            .then((workout) => {
-                res.json(workout)
-            })
-            .catch((err) => {
-                res.json(err)
-            })
-});
-router.put("/api/workouts/:id" ,(req, res) => {
-    console.log('PARAMS', req.params)
-    Workout.findByIdAndUpdate(
-        req.params.id,
-        {$push: {exercise: req.body}},
-        {new:true, runValidators: true}
-    )
-    .then((workout) => {
-        res.json(workout)
-    })
-    .catch((e) =>{
-        res.json(e)
-    })
-});
-router.get(`/api/workouts/range`, (req, res) => {
+router.get("/workouts", (req, res) => {
     Workout.aggregate([
         {
             $addFields: {
@@ -54,13 +21,50 @@ router.get(`/api/workouts/range`, (req, res) => {
             },
         },
     ])
-    .then((workout) => {
-        console.log('YOO', workout);
-        res.json(workout)
-    })
-    .catch((e) => {
-        res.json(e)
-    })
-  });
+        .then((workout) => {
+            console.log('YOO', workout);
+            res.json(workout)
+        })
+        .catch((e) => {
+            res.json(e)
+        })
+});
+
+router.put("/workouts/:id", ({ params, body }, res) => {
+    console.log('PARAMS', req.params)
+    Workout.findByIdAndUpdate(
+        { _id: params.id },
+        { $push: { exercise: body } },
+        { new: true, runValidators: true }
+    )
+        .then((workout) => {
+            res.json(workout)
+        })
+        .catch((e) => {
+            res.json(e)
+        })
+});
+router.get("/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration:
+                {
+                    $sum: '$exercise.duration'
+                },
+                totalWeight:
+                    { $sum: '$exercises.weight' }
+            }
+        }
+    ])
+        .limit(10)
+        .then((workout) => {
+            console.log('YOO', workout);
+            res.json(workout)
+        })
+        .catch((e) => {
+            res.json(e)
+        })
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -21,10 +21,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
- app.use(require("./routes/api.js"));
- app.use(require("./routes/htmlRoutes.js"));
-
-
+ app.use(router);
+ 
 app.listen(PORT, () => {
   console.log(`Now listening ${PORT}!`);
 });
